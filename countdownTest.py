df6a1886b1e80ef2e1ce706aa85e0f9402ba895d
Alexander Hedling <taikes_bass@hotmail.com>
2021-01-06 01:06:04 +0100

    bruteforce tillagd - hittar lösningar för 10, 7 och 5 poäng

--- countdownTest.py
+++ countdownTest.py
@@ -3,7 +3,7 @@
 import random
 
 
-
+#### FUNKTIONER ####
 
 def randomLarge(choice=0):
     largeNums = [25, 50, 75, 100] # 0-4 nummer väljs, antalet + smånumren ska bli totalt 6 st
@@ -30,19 +30,61 @@ def randomTarget():
     # Ger inga siffror under 100, ex 045 etc
     return random.randrange(100, 999, 1)
 
+def possibleSolutions(numbers):
+    n1, n2, n3, n4, n5, n6 = numbers
 
-def solve(target, numbers, solution):
-    if len(numbers) == 0:
-        print("ingen lösning")
-        return
+    # brute force
+    solutions = []
+    operand = ["+", "-", "/", "*"]
+    for one in range(4):
+        for two in range(4):
+            for three in range(4):
+                for four in range(4):
+                    for five in range(4):
+                        #onödigt att lägga detta separat, kan utvärdera direkt
+                        example = str(n1) + str(operand[one]) + str(n2) + str(operand[two]) + str(n3) + str(operand[three]) + str(n4) + str(operand[four]) + str(n5) + str(operand[five]) + str(n6)
+                        solutions.append(example)
+                        print("Går igenom uträkning: " + example)
+
+    return solutions
+
+def solve(target, numbers):
+    evauationOfCalculations = {
+        "10p": [],
+        "7p": [],
+        "5p": []
+    }
+
+    sevenLow = target - 5
+    sevenHigh = target + 5
+    fiveLow = target - 10
+    fiveHigh = target + 10
 
 
+    for calculation in numbers:
+        answer = eval(calculation)
+        if answer == target:
+            print(calculation + " ger svaret " + str(target))
+            evauationOfCalculations["10p"].append(calculation)
+        elif answer >= sevenLow and answer <= sevenHigh:
+            print(calculation + " är 5 eller mindre ifrån " + str(target))
+            evauationOfCalculations["7p"].append(calculation)
+        elif answer >= fiveLow and answer <= fiveHigh:
+            print(calculation + " är 10 eller mindre ifrån " + str(target))
+            evauationOfCalculations["5p"].append(calculation)
 
+    return evauationOfCalculations
+
+
+#### MAIN ####
 choice = 1
 randomNums = randomLarge(choice) + randomSmall(6 - choice)
 target = randomTarget()
+possibleCalculations = possibleSolutions(randomNums)
+#solve(target, possibleCalculations)
+
+print(solve(target, possibleCalculations))
+
 
 print(target)
 print(randomNums)
-
-solve(target, randomNums, "")
